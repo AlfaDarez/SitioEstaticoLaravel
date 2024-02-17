6cd048481d4706b803dba6453cfe23a24b72aac0
Jose Alfredo Valladares <josevalladaresvillanueva@gmail.com>
2024-02-17 16:20:30 -0600

    Agregando SitioController y las opciones para registro de usuario

---
+++ app/Http/Controllers/SitioController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SitioController extends Controller
+{
+    //valida las funciones de ingreso al sitio
+
+    //cuando ingresan al index
+    public function index(){
+
+    }
+
+
+    //cuando ingresan a la pagína de contacto
+    public function contacto(){
+
+
+    }
+
+    //cuando ingresan a la pagína de nosotros
+    public function nosotros(){
+
+    }
+
+    //cuando ingresan a la pagína de productos
+    public function productos(){
+
+    }
+
+    //cuando ingresan a la pagína de comprar enviara a whatssapp
+    public function comprar($data){
+
+    }
+
+}
